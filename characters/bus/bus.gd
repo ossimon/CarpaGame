08c9fc26383529441baf9bca5befddae4dc68cff
Oskar Simon <oskarsimon3@gmail.com>
2025-04-12 20:13:20 +0200

    bus escapes

--- characters/bus/bus.gd
+++ characters/bus/bus.gd
@@ -1,5 +1,17 @@
 extends Sprite2D
 
 
-func _on_bus_start_actuator_area_entered(area: Area2D) -> void:
-	print('entered')
+@export var speed = 200
+
+var velocity = Vector2.ZERO
+
+
+func _on_bus_start_actuator_body_entered(body):
+	velocity = Vector2.RIGHT * speed
+
+
+func _process(delta):
+	position += velocity * delta
+	if position.x >= 2000:
+		# delete this node
+		queue_free()
